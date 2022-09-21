1e051a0f5d56a54aa1f12bd666f7e8d032087ad6
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-21 12:22:04 +0530

    Expressing actions as Type Union

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Todo, fetchTodos } from '../actions';
-import { StoreState } from '../reducers.ts';
+import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[]
